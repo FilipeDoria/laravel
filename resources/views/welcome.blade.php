8f2a689b349057910c2918283ef6e38a9eaaeea9
Filipe Dória <filipe0doria@gmail.com>
2020-09-04 16:01:01 +0100

    Home file update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Doria Apps
                 </div>
 
                 <div class="links">
